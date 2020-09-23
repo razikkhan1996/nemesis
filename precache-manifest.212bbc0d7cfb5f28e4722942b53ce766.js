66b1652ea008d20418d0c695c2889fcde1fea95b
Razik <razik99815@gmail.com>
2020-09-23 14:01:35 +0530

    Updates

---
+++ precache-manifest.212bbc0d7cfb5f28e4722942b53ce766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce6136eeb86c18d13986a7b96e7155e",
+    "url": "/nemesis/index.html"
+  },
+  {
+    "revision": "4c5ea10ca6ffb151a542",
+    "url": "/nemesis/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9a965ef46c5bb53afcaa",
+    "url": "/nemesis/static/css/main.2c6551bc.chunk.css"
+  },
+  {
+    "revision": "4c5ea10ca6ffb151a542",
+    "url": "/nemesis/static/js/2.07afca60.chunk.js"
+  },
+  {
+    "revision": "0adf7f1b9434a120a5bce5b5824fa305",
+    "url": "/nemesis/static/js/2.07afca60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a965ef46c5bb53afcaa",
+    "url": "/nemesis/static/js/main.4f7976d1.chunk.js"
+  },
+  {
+    "revision": "229e483927f93f39eaa4",
+    "url": "/nemesis/static/js/runtime-main.0f1ea70d.js"
+  }
+]);
